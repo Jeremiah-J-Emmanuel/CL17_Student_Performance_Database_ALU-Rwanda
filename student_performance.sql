0cb082ab2076270bdebe49097f5c9d0c7946c071
Jeremiah-J-Emmanuel <jerryjewelnuel2@gmail.com>
2025-08-21 22:55:59 +0200

    Commit

--- student_performance.sql
+++ student_performance.sql
@@ -89,8 +89,33 @@ INSERT INTO python_grades (course_id, course_name, student_id, grade_obtained) V
 (318, 'Python Programming', 1013, 38),
 (319, 'Python Programming', 1019, 64),
 (320, 'Python Programming', 1025, 88);
+
+-- Number 2
 SELECT s.student_id, s.student_name, s.intake_year, lg.grade_obtained as linux_grade
 FROM students s
 JOIN linux_grades lg ON s.student_id = lg.student_id
 WHERE CAST(lg.grade_obtained AS UNSIGNED) < 50
 ORDER BY CAST(lg.grade_obtained AS UNSIGNED) ASC;
+
+--Number 3
+
+-- Students that took only Linux
+SELECT s.student_id, s.student_name, lg.course_name, lg.grade_obtained
+FROM students s
+JOIN linux_grades lg ON s.student_id = lg.student_id
+WHERE s.student_id NOT IN (
+    SELECT student_id FROM python_grades
+);
+
+-- Students that took only Python
+SELECT s.student_id, s.student_name, pg.course_name, pg.grade_obtained
+FROM students s
+JOIN python_grades pg ON s.student_id = pg.student_id
+WHERE s.student_id NOT IN (
+    SELECT student_id FROM linux_grades
+);
+
+--Number 4
+-- This is for Keira
+
+--Number 5
